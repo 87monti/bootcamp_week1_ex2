8f52b2e50fc6cc058ec1d61b923b13e17e020824
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2016-12-16 11:21:37 +0100

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,82 @@
+/*  Zad 1
+
+
+(function($) {
+
+$(document).ready(function() {
+
+
+	var par = $('div.grid, div.grid-12');
+
+	par.hl();
+})
+})(jQuery);
+
+*/ 
+
+
+
+//Zadanie 2
+
+ /* (function($){
+
+
+$(document).ready(function(){
+
+
+
+var href = $(".nav a[href^='http']");
+
+
+href.hl();
+
+
+})
+})(jQuery);
+
+*/
+
+//Zadanie 3
+/*
+
+ (function($){
+
+
+$(document).ready(function(){
+
+var sa = $("input:radio, input:checkbox").not(":checked");
+console.log(sa);
+
+
+})
+})(jQuery);
+
+*/
+// Zadanie 4
+
+/* 
+(function($){
+
+
+$(document).ready(function(){
+
+var par = $("#text p:empty:first");
+par.hl();
+
+})
+}) (jQuery);
+
+
+*/ 
+//Zadanie 5
+/* (function($){
+
+$(document).ready(function(){
+
+var sp= $(".pagination-item:not(span)");
+sp.hl();
+	
+
+})
+}) (jQuery);
+*/
